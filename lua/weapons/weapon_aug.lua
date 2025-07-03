9f9d38af20f403ab2546317ab376e8b1c807f487
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-07-03 04:08:24 +0300

    Add AUG

---
+++ lua/weapons/weapon_aug.lua
@@ -0,0 +1,32 @@
+AddCSLuaFile()
+
+SWEP.Base = "weapon_csbase_scoped"
+SWEP.Spawnable = true
+
+SWEP.PrintName = "AUG"
+SWEP.Category = "Counter-Strike: Source2"
+SWEP.Slot = 2
+
+SWEP.ViewModel = "models/weapons/cstrike/c_rif_aug.mdl"
+SWEP.WorldModel = "models/weapons/w_rif_aug.mdl"
+SWEP.HoldType = "smg"
+SWEP.UseHands = true
+
+SWEP.ScopeZoom = {3}
+SWEP.UseScope = false
+SWEP.Primary.Sound = "Weapon_AUG.Single"
+SWEP.Primary.Damage = 32
+SWEP.Primary.NumShots = 1
+SWEP.Primary.Delay = 60 / 666
+SWEP.Primary.ClipSize = 30
+SWEP.Primary.DefaultClip = 30
+SWEP.Primary.Automatic = true
+SWEP.Primary.Ammo = "AR2"
+
+SWEP.Primary.Cone = 0.0078
+SWEP.Primary.Recoil = {}
+SWEP.Primary.Recoil.MinAng = Angle(0.5, -0.2, 0)
+SWEP.Primary.Recoil.MaxAng = Angle(0.6, 0.2, 0)
+SWEP.Primary.Recoil.Punch = 0.4
+
+list.Add("NPCUsableWeapons", {class = "weapon_aug", title = SWEP.PrintName})
