1ba8cf658b9b6d72f4972b824ff467b7886226d1
TheLovelyMask <118691267+TheLovelyMask@users.noreply.github.com>
2025-11-06 17:20:02 +0200

    Adjust M249 weapon stats and parameters
    Reduced damage and adjusted clip size, delay, and spread parameters for the M249 weapon.

--- lua/weapons/weapon_m249.lua
+++ lua/weapons/weapon_m249.lua
@@ -13,17 +13,19 @@ SWEP.WorldModel = "models/weapons/w_mach_m249para.mdl"
 SWEP.HoldType = "ar2"
 
 SWEP.Primary.Sound = "Weapon_M249.Single"
-SWEP.Primary.Damage = 35
+SWEP.Primary.Damage = 25
 SWEP.Primary.NumShots = 1
-SWEP.Primary.Delay = 0.08
-SWEP.Primary.ClipSize = 100
-SWEP.Primary.DefaultClip = 200
+SWEP.Primary.Delay = 0.11
+SWEP.Primary.ClipSize = 60
+SWEP.Primary.DefaultClip = 120
 SWEP.Primary.Automatic = true
 SWEP.Primary.Ammo = "AR2"
 
-SWEP.Primary.Cone = 0.018
-SWEP.Primary.Recoil = {MinAng = Angle(0.25, -0.2, 0), MaxAng = Angle(0.35, 0.2, 0)}
-SWEP.NPCBurstData = {Min = 10, Max = 20, Delay = SWEP.Primary.Delay}
+SWEP.Primary.Cone = 0.03
+SWEP.Primary.Recoil = {MinAng = Angle(1.2, -0.6, 0), MaxAng = Angle(1.6, 0.6, 0)}
+SWEP.Primary.SpreadIncrease = 0.003
+SWEP.Primary.MaxSpread = 0.06
+SWEP.NPCBurstData = {Min = 4, Max = 8, Delay = SWEP.Primary.Delay}
 
 if CLIENT then
 	killicon.AddFont("weapon_m249", "css_sweps_killicons", "z", Color(255, 80, 0), 0.4)
