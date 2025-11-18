e6b2f2ffeb27b4d626a7e0c6808ace11369da3e9
Astralcircle <142503363+Astralcircle@users.noreply.github.com>
2025-11-19 01:22:29 +0300

    Try to balase M249

--- lua/weapons/weapon_m249.lua
+++ lua/weapons/weapon_m249.lua
@@ -15,16 +15,14 @@ SWEP.HoldType = "ar2"
 SWEP.Primary.Sound = "Weapon_M249.Single"
 SWEP.Primary.Damage = 25
 SWEP.Primary.NumShots = 1
-SWEP.Primary.Delay = 0.11
-SWEP.Primary.ClipSize = 60
-SWEP.Primary.DefaultClip = 120
+SWEP.Primary.Delay = 0.08
+SWEP.Primary.ClipSize = 100
+SWEP.Primary.DefaultClip = 200
 SWEP.Primary.Automatic = true
 SWEP.Primary.Ammo = "AR2"
 
 SWEP.Primary.Cone = 0.03
 SWEP.Primary.Recoil = {MinAng = Angle(1.2, -0.6, 0), MaxAng = Angle(1.6, 0.6, 0)}
-SWEP.Primary.SpreadIncrease = 0.003
-SWEP.Primary.MaxSpread = 0.06
 SWEP.NPCBurstData = {Min = 4, Max = 8, Delay = SWEP.Primary.Delay}
 
 if CLIENT then
